bd997c8840d4b3a1d88781e334cf9ac1600afdb5
Kyle <khartson2017@gmail.com>
2022-09-21 13:11:33 -0400

    commit tree f3e9b09de5ef86e451ab0ea297ce64e404ca629c

--- src/features/bands/BandInput.js
+++ src/features/bands/BandInput.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function BandInput() {
-  return <div>Band Input</div>;
+function BandInput({ onBandSubmit }) {
+  const [name, setName] = useState("");
+ 
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onBandSubmit(name)
+    setName("");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name
+          <input type='text' name='name' value={name} onChange={handleChange}/>
+          <input value='add band' type='submit'/>
+        </label>
+      </form>
+    </div>
+  );
 }
 
 export default BandInput;
