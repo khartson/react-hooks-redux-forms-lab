c530b1beea340b6a4bae0c651d245cee4ca16d12
Kyle <khartson2017@gmail.com>
2022-09-21 13:11:36 -0400

    completed assignment

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { bandAdded } from './bandsSlice';
+
+import BandInput from "./BandInput";
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+  const dispatch = useDispatch();
+  const bands = useSelector((state) => state.bands.entities);
+  
+  function handleBandSubmit(name) {
+    dispatch(bandAdded(name));
+  }
+  return(
+  <div>
+      <BandInput onBandSubmit={handleBandSubmit}/>
+      <ul>
+        {bands.map((band)=> {
+          return <li key={band}>{band}</li>
+        })}
+      </ul>
+  </div>
+  );
 }
-
 export default BandsContainer;
